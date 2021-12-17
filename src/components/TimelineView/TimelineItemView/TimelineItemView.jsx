f79dc715b6fc609d788eac8d6eadb057edf56ccc
parth-commits <parthpatel1087@gmail.com>
2021-12-17 18:55:09 -0500

    made timeline items minimizable

--- src/components/TimelineView/TimelineItemView/TimelineItemView.jsx
+++ src/components/TimelineView/TimelineItemView/TimelineItemView.jsx
@@ -1,15 +1,19 @@
 import React, { Component } from "react";
 import './TimelineItemView.css'
 class TimelineItemView extends Component {
-    
+    state = { open: this.props.item.isExpanded }
+
+    handleClick = () => {
+        this.setState({ open: !this.state.open })
+    }
     render() {
         return(
             <div className="timeline-item fade-in">
-                <label className="timeline-item-title">
-                    <h3 className="timeline-item-title-title">{this.props.item.position}, {this.props.item.company}</h3>
+                <label className={`timeline-item-title ${this.state.open ? 'timeline-content-open' : 'timeline-content-closed'}`} onClick={this.handleClick}>
+                    <h3 className="timeline-item-title-title"><i className={`fas fa-caret-right ${this.state.open ? 'timeline-caret-open' : 'timeline-caret-closed'}`}></i>{this.props.item.position}, {this.props.item.company}</h3>
                     <p className="timeline-item-title-date">{this.props.item.time}</p>
                 </label>
-                <div className="timeline-item-content">
+                <div className={`timeline-item-content ${this.state.open ? 'timeline-content-open' : 'timeline-content-closed'}`}>
                     <ul>
                         {this.props.item.descriptionItems.map((item, index) => {
                             return (
--- src/components/TimelineView/TimelineItems.js
+++ src/components/TimelineView/TimelineItems.js
@@ -3,6 +3,7 @@ export const TimelineItems = [
         position: 'AR Applications Development Engineer',
         company: 'ModiFace - a L\'Oréal Company',
         time: 'May 2021 - Present',
+        isExpanded: true,
         descriptionItems: [
             'Actively involved in the development and maintenance of the core SDK, expanding it with features to provide a better User Experience.',
             'Solved critical errors for multiple products (e.g., Virtual Try On SDK) by creating automated tests, fixing bugs, and improving existing code base with enhanced programming principles which provided an error-free experience to millions of customers of top makeup brands worldwide including but not limited to L’Oréal, Maybelline and NYX.',
@@ -14,6 +15,7 @@ export const TimelineItems = [
         position: 'Private Watch App Developer',
         company: 'Samsung Galaxy Store',
         time: 'June 2021 - Present',
+        isExpanded: true,
         descriptionItems: [
             'Followed the Software Development Process model to create and maintain multiple apps.',
             'Created multiple apps that aid in making the user’s daily life activities more convenient. For example, made a <a href="https://galaxy.store/starbucks" target="_blank"> <b> Starbucks Rewards App</b></a> - with currently over <b class="confetti-items">9000</b> users - which allows them to scan their Rewards Card right from their watch, so they don’t have to pull out their phone at the Café. Then made a follow up app - <a href="https://galaxy.store/sqadoxczl" target="_blank"> <b> SB Scan + Pay</b></a> - which allows users to use their Starbucks Card to Pay for their beverage all the while getting rewards, right from their watch!',
@@ -25,6 +27,7 @@ export const TimelineItems = [
         position: 'Lead Instructor',
         company: 'UCMAS',
         time: 'March - June 2019',
+        isExpanded: false,
         descriptionItems: [
             'An instructor for students aged between 8-16. Explained mathematical algorithmic concepts to young individuals along with teaching them new concepts and critical thinking skills.',
             'Created new and effective learning material to help drastically improve students results in school',
@@ -34,6 +37,7 @@ export const TimelineItems = [
         position: 'Math and Science Tutor',
         company: 'Licensed to Learn (L2L)',
         time: 'February - June 2019',
+        isExpanded: false,
         descriptionItems: [
             'Monitored, assessed, and remediated their performance, which strengthened their understanding of math and sciences to help significantly improve their grades',
         ],
@@ -42,6 +46,7 @@ export const TimelineItems = [
         position: 'Science Challenge Executive',
         company: 'North Albion C.I.',
         time: 'February 2016 - November 2019',
+        isExpanded: false,
         descriptionItems: [
             'Organized and managed a science competition by coordinating with other volunteers and teachers',
             'Managed and prepared a team of 100 high school students to help facilitate the event',
